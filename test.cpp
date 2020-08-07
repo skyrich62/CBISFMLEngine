4552f9e1274b0e024b73790dabc373a88579a77e
Rich Newman <skyrich62@nraonline.com>
2020-08-07 16:25:12 -0700

    Refactor test program to use an encapsulated impl (Block)

--- test.cpp
+++ test.cpp
@@ -4,6 +4,31 @@
 #include <SFML/Graphics.hpp>
 
 namespace cbisf = CompuBrite::SFML;
+namespace cbi = CompuBrite;
+
+class Block
+{
+public:
+    Block(const std::string &filename)
+    {
+        if (!texture_.loadFromFile(filename)) {
+            cbi::CheckPoint::hit(CBI_HERE, "Can't load ", filename);
+            return;
+        }
+        sprite_.setTexture(texture_);
+    }
+
+    cbisf::EntityImpl impl{
+        [this](sf::RenderTarget& target, sf::RenderStates states)
+        {
+            target.draw(sprite_, states);
+        },
+        [this]() { return sprite_.getLocalBounds(); }
+    };
+private:
+    sf::Sprite sprite_;
+    sf::Texture texture_;
+};
 
 int main()
 {
@@ -11,22 +36,13 @@ int main()
     sf::RenderWindow app(sf::VideoMode(800, 600), "SFML window");
     app.setFramerateLimit(60);
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("cb.bmp"))
-        return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
-    cbisf::EntityImpl impl{
-        [&sprite](sf::RenderTarget& target, sf::RenderStates states)
-        {
-            target.draw(sprite, states);
-        },
-        [&sprite]() { return sprite.getLocalBounds(); }
-    };
-    cbisf::Entity entity(impl);
+    Block block("cb.bmp");
+    cbisf::Entity entity(block.impl);
     auto size = entity.getLocalBounds();
     entity.setOrigin(size.width / 2.0, size.height / 2.0);
     entity.move(400.0, 300.0);
+
+    cbi::CheckPoint dbg("entity");
 
 	// Start the game loop
     while (app.isOpen())
@@ -40,6 +56,7 @@ int main()
                 app.close();
         }
         entity.rotate(1.0);
+
         // Clear screen
         app.clear();
 
